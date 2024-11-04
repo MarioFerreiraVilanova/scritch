9ab184b3d0fb6c1e4f761b6706469c13e00fb150
marioprivate <mario.ferreira.vilanova@gmail.com>
2024-11-04 08:45:19 +0100

    Created a generic loading indicator

--- composeApp/src/commonMain/kotlin/app/minimal/fasting/fasting/status/ui/FastingStatusScreen.kt
+++ composeApp/src/commonMain/kotlin/app/minimal/fasting/fasting/status/ui/FastingStatusScreen.kt
@@ -4,9 +4,13 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import app.minimal.fasting.common.ui.LoadingScreen
 import app.minimal.fasting.fasting.status.FastingStatusViewModel
+import app.minimal.fasting.fasting.status.FastingStatusViewState
 import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
@@ -14,6 +18,18 @@ fun FastingStatusScreen(
     modifier: Modifier = Modifier,
     viewModel: FastingStatusViewModel = koinViewModel<FastingStatusViewModel>()
 ) {
+    val viewState by viewModel.viewState.collectAsState()
+
+    when (viewState){
+        FastingStatusViewState.Loading -> LoadingScreen()
+        is FastingStatusViewState.Loaded -> Loaded()
+    }
+}
+
+@Composable
+private fun Loaded(
+    modifier: Modifier = Modifier,
+){
     Box(
         modifier = modifier.fillMaxSize()
     ) {
