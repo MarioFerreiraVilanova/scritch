b55a869cc9217062dccf6c712d1a2adfc9b1dbac
marioprivate <mario.ferreira.vilanova@gmail.com>
2025-02-23 10:49:09 -0600

    Basic styling of tips sheets

--- composeApp/src/commonMain/kotlin/com/scritch/app/home/Tips.kt
+++ composeApp/src/commonMain/kotlin/com/scritch/app/home/Tips.kt
@@ -1,43 +1,33 @@
 package com.scritch.app.home
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextLinkStyles
-import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.fromHtml
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Tips(
     title: String,
     description: String,
 ) {
-    Column(
-        modifier = Modifier.navigationBarsPadding()
-    ) {
-        TopAppBar(
-            title = {
-                Text("Prompt tips")
-            }
-        )
-        if (description.firstOrNull() == '<'){
+    Column {
+        if (description.firstOrNull() == '<') {
             Text(
                 text = AnnotatedString.fromHtml(
                     description,
                     linkStyles = TextLinkStyles(
                         style = SpanStyle(
                             textDecoration = TextDecoration.Underline,
+                            color = MaterialTheme.colorScheme.primary
                         )
                     )
                 ),
@@ -46,7 +36,9 @@ fun Tips(
         } else {
             Text(
                 text = title,
-                style = MaterialTheme.typography.headlineSmall,
+                style = MaterialTheme.typography.headlineMedium,
+                fontWeight = FontWeight.Bold,
+                color = MaterialTheme.colorScheme.primary,
                 modifier = Modifier.padding(
                     top = 16.dp,
                     start = 16.dp,
