8ade192ff0ce751763ae84b2f58cf137bea9c6b1
marioprivate <mario.ferreira.vilanova@gmail.com>
2025-04-22 10:11:33 +0200

    Added a CTA to the about page

--- composeApp/src/commonMain/kotlin/com/scritch/app/settings/about/AboutScreen.kt
+++ composeApp/src/commonMain/kotlin/com/scritch/app/settings/about/AboutScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -96,6 +97,28 @@ fun AboutScreen(
                     style = MaterialTheme.typography.bodyLarge,
                     color = MaterialTheme.colorScheme.onSurface,
                 )
+            }
+            item {
+                Spacer(
+                    modifier = Modifier.height(16.dp)
+                )
+            }
+            item {
+                Text(
+                    text = "Feedback, suggestions?",
+                    style = MaterialTheme.typography.bodyLarge,
+                    color = MaterialTheme.colorScheme.onSurface,
+                )
+            }
+            item {
+                OutlinedButton(
+                    onClick = {},
+                ) {
+                    Text(
+                        text = "Send us an email!",
+                        style = MaterialTheme.typography.bodyLarge,
+                    )
+                }
             }
             item {
                 Spacer(
