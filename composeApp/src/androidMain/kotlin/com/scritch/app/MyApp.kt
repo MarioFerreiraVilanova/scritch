14b85af2f50645bd443fe7f3cbda73e533acb18e
marioprivate <mario.ferreira.vilanova@gmail.com>
2025-05-21 08:42:09 +0200

    Application context provided

--- composeApp/src/androidMain/kotlin/com/scritch/app/MyApp.kt
+++ composeApp/src/androidMain/kotlin/com/scritch/app/MyApp.kt
@@ -1,10 +1,12 @@
 package com.scritch.app
 
 import android.app.Application
+import com.scritch.app.app.ApplicationContext
 import org.koin.core.component.KoinComponent
 
 class MyApp : Application(), KoinComponent {
     override fun onCreate() {
         super.onCreate()
+        ApplicationContext.setUp(applicationContext)
     }
 }
