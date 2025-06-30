2d29b4ef61517d847ad743606b9dc74da1279089
marioprivate <mario.ferreira.vilanova@gmail.com>
2025-06-30 14:27:05 +0200

    0.12

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -101,8 +101,8 @@ android {
         applicationId = "com.scritch.app"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 11
-        versionName = "0.11"
+        versionCode = 12
+        versionName = "0.12"
     }
     packaging {
         resources {
