bfb79b06f987a31b1c7299327a9786f6c8a5299d
marioprivate <mario.ferreira.vilanova@gmail.com>
2025-08-04 15:22:56 +0200

    Jams are loading and being shown!

--- composeApp/src/commonMain/kotlin/com/scritch/app/jam/JamScreen.kt
+++ composeApp/src/commonMain/kotlin/com/scritch/app/jam/JamScreen.kt
@@ -1,28 +1,77 @@
 package com.scritch.app.jam
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.scritch.app.prompt.Prompt
+import com.scritch.app.prompt.TipsSheet
 import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
 fun JamScreen(
     modifier: Modifier = Modifier,
     viewModel: JamViewModel = koinViewModel(),
-){
-    Scaffold { contentPadding ->
+) {
+    val viewState by viewModel.viewState.collectAsState()
+    Scaffold(
+        modifier = modifier,
+    ) { contentPadding ->
         Box(
-            modifier = Modifier.fillMaxSize().padding(contentPadding),
-            contentAlignment = Alignment.Center,
-        ) {
-            Text(
-                "Weekly jam screen"
-            )
+            modifier = modifier
+                .fillMaxSize()
+                .consumeWindowInsets(contentPadding)
+                .padding(contentPadding),
+        ){
+            Column(
+                verticalArrangement = Arrangement.spacedBy(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.padding(16.dp),
+            ) {
+                Text(
+                    text = "Weekly jams are drawing challenges for all Scritch users. Updated every Sundays.",
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = MaterialTheme.colorScheme.onBackground,
+                )
+                HorizontalDivider()
+                when (viewState.loadingState){
+                    LoadingState.LOADING -> {
+                        CircularProgressIndicator()
+                    }
+                    LoadingState.NO_JAM -> {
+                        Text(
+                            text = "No jams available",
+                            style = MaterialTheme.typography.bodyMedium,
+                            color = MaterialTheme.colorScheme.onBackground,
+                        )
+                    }
+                    LoadingState.LOADED,
+                    LoadingState.REFRESHING -> {
+                        Prompt(
+                            viewState = viewState.promptViewState,
+                            onCategoryClick = viewModel::onCategoryClick,
+                        )
+                    }
+                }
+            }
         }
     }
+
+    TipsSheet(
+        viewState = viewState.promptViewState,
+        onTipDisplayed = viewModel::onTipDisplayed,
+    )
 }
