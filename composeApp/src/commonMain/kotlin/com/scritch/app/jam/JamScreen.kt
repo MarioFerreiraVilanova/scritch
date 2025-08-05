c52711084cada7527693a4334396566b35a0c0b8
marioprivate <mario.ferreira.vilanova@gmail.com>
2025-08-05 09:38:32 +0200

    Updated header of jam screen

--- composeApp/src/commonMain/kotlin/com/scritch/app/jam/JamScreen.kt
+++ composeApp/src/commonMain/kotlin/com/scritch/app/jam/JamScreen.kt
@@ -2,26 +2,31 @@ package com.scritch.app.jam
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.scritch.app.prompt.Prompt
 import com.scritch.app.prompt.TipsSheet
 import com.scritch.app.util.dayOfWeekString
+import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 import org.koin.compose.viewmodel.koinViewModel
 import scritch.composeapp.generated.resources.Res
+import scritch.composeapp.generated.resources.clock
 import scritch.composeapp.generated.resources.weekly_jam_description
 import scritch.composeapp.generated.resources.weekly_jam_end_time
 import scritch.composeapp.generated.resources.weekly_jam_not_available
@@ -53,11 +58,20 @@ fun JamScreen(
             viewState.endDate?.let { endDate ->
                 item {
                     val dayOfWeek = dayOfWeekString(day = endDate.dayOfWeek)
-                    Text(
-                        text = stringResource(Res.string.weekly_jam_end_time, dayOfWeek, "${endDate.hour}:${endDate.minute}"),
-                        style = MaterialTheme.typography.bodyMedium,
-                        color = MaterialTheme.colorScheme.onBackground,
-                    )
+                    Row(
+                        horizontalArrangement = Arrangement.spacedBy(8.dp),
+                        verticalAlignment = Alignment.CenterVertically,
+                    ){
+                        Icon(
+                            painter = painterResource(Res.drawable.clock),
+                            contentDescription = null,
+                        )
+                        Text(
+                            text = stringResource(Res.string.weekly_jam_end_time, dayOfWeek, "${endDate.hour}:${endDate.minute}"),
+                            style = MaterialTheme.typography.bodyMedium,
+                            color = MaterialTheme.colorScheme.onBackground,
+                        )
+                    }
                 }
             }
             item {
